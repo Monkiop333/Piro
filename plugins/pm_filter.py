1ee14edc10833b4c1d8dac06051069277ddcc4b4
Guardianff <103484627+Guardianff@users.noreply.github.com>
2024-05-26 11:16:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -257,32 +257,30 @@ async def next_page(bot, query):
 
 @Client.on_callback_query(filters.regex(r"^spol"))
 async def advantage_spoll_choker(bot, query):
-    _, user, movie_ = query.data.split('#')
-    movies = SPELL_CHECK.get(query.message.reply_to_message.id)
-    if not movies:
-        return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+    _, user, movie_index = query.data.split('#')
+    corrected_movies = SPELL_CHECK.get(query.message.reply_to_message.id)
+    if not corrected_movies:
+        return await query.answer("The request has already been processed or is old.", show_alert=True)
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-    if movie_ == "close_spellcheck":
+        return await query.answer("You do not have permission to perform this action.", show_alert=True)
+    if movie_index == "close_spellcheck":
         return await query.message.delete()
-    movie = movies[(int(movie_))]
-    await query.answer(script.TOP_ALRT_MSG)
-    gl = await global_filters(bot, query.message, text=movie)
-    if gl == False:
-        k = await manual_filters(bot, query.message, text=movie)
-        if k == False:
-            files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
-            if files:
-                k = (movie, files, offset, total_results)
-                await auto_filter(bot, query, k)
-            else:
-                reqstr1 = query.from_user.id if query.from_user else 0
-                reqstr = await bot.get_users(reqstr1)
-                if NO_RESULTS_MSG:
-                    await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                k = await query.message.edit(script.MVE_NT_FND)
-                await asyncio.sleep(10)
-                await k.delete()
+
+    corrected_movie = corrected_movies[int(movie_index)]
+    await query.answer("In Progress...", cache_time=60)
+
+    files, offset, total_results = await get_search_results(query.message.chat.id, corrected_movie, offset=0, filter=True)
+    if files:
+        await auto_filter(bot, query, (corrected_movie, files, offset, total_results))
+    else:
+        reqstr1 = query.from_user.id if query.from_user else 0
+        reqstr = await bot.get_users(reqstr1)
+        await bot.send_message(chat_id=LOG_CHANNEL, text=f"No result: {reqstr.id}, {reqstr.mention}, {corrected_movie}")
+        await query.message.edit("Can't find the movie.")
+        await asyncio.sleep(10)
+        await query.message.delete()
+
+            
 
 
 @Client.on_callback_query()
